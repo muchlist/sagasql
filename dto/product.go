6b4d6e8ee609794bbda74e49af4f4fa6595a9c5f
muchlist <whois.muchlis@gmail.com>
2021-08-18 21:23:18 +0800

    simple nullable string

--- dto/product.go
+++ dto/product.go
@@ -6,7 +6,7 @@ type Product struct {
 	Price     int64           `json:"price"`
 	CreatedBy string          `json:"created_by"`
 	CreatedAt int64           `json:"crated_at"`
-	Image     string          `json:"image"`
+	Image     *string         `json:"image"`
 }
 
 type ProductReq struct {
